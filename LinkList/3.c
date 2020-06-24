28f0913a11b35481a666358d5bd8fa7f02b5ee4b
hpumengzhao <36286293+hpumengzhao@users.noreply.github.com>
2020-06-24 21:32:05 +0800

    Create 3.c

---
+++ LinkList/3.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct LNode{
+	int val;
+	struct LNode *next;
+}LNode,*LinkList;
+
+/*
+	Create a LinkList with head node
+*/
+void Construct(LinkList &L,int n){
+	L=(LinkList)malloc(sizeof(LinkList));
+	LNode *s,*r;
+	s=L;
+	s->next=NULL;
+
+	int x;
+	for(int i=1;i<=n;i++){
+		scanf("%d",&x);
+
+		r=(LNode*)malloc(sizeof(LNode));
+		r->val=x;
+		s->next=r;
+		s=r;
+	}
+	s->next=NULL;
+}
+
+/*
+	Print LinkList with head
+*/
+void OutPut(LinkList L){
+	while(L->next!=NULL){
+		printf("%d ", L->next->val);
+		L=L->next;
+	}
+	puts("");
+}
+
+/*
+	Print LinkList from tail to head
+*/
+void Print(LinkList L){
+	if(L==NULL) return ;
+	Print(L->next);
+	printf("%d ", L->val);
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	LinkList L;
+	Construct(L,n);
+	Print(L);
+	puts("");
+	return 0;
+}
